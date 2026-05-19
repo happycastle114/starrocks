35f472695ee49d4bab23f65a67f1dd803c4dc706
Denis Gribanov <itsme@petrikoro.com>
2026-05-19 07:06:03 +0300

    feat(auth): support Open Policy Agent authorization
    Signed-off-by: Denis Gribanov <itsme@petrikoro.com>

--- fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
+++ fe/fe-core/src/main/java/com/starrocks/server/GlobalStateMgr.java
@@ -54,6 +54,7 @@ import com.starrocks.authorization.AuthorizationMgr;
 import com.starrocks.authorization.DefaultAuthorizationProvider;
 import com.starrocks.authorization.NativeAccessController;
 import com.starrocks.authorization.PrivilegeException;
+import com.starrocks.authorization.opa.OpaAccessController;
 import com.starrocks.authorization.ranger.starrocks.RangerStarRocksAccessController;
 import com.starrocks.backup.BackupHandler;
 import com.starrocks.binlog.BinlogManager;
@@ -857,6 +858,8 @@ public class GlobalStateMgr {
         AccessControlProvider accessControlProvider;
         if (Config.access_control.equals("ranger")) {
             accessControlProvider = new AccessControlProvider(new AuthorizerStmtVisitor(), new RangerStarRocksAccessController());
+        } else if (Config.access_control.equals("opa")) {
+            accessControlProvider = new AccessControlProvider(new AuthorizerStmtVisitor(), new OpaAccessController());
         } else {
             accessControlProvider = new AccessControlProvider(new AuthorizerStmtVisitor(), new NativeAccessController());
         }
